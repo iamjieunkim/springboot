e9fad67f59a9e3c8dfdc34903dccb9e768824c76
iamjieunkim <jek3118@naver.com>
2022-11-05 16:03:26 +0900

    첫 번째

---
+++ src/test/java/com/example/abo/AboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
